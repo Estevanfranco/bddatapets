51b612315a7b08c1e2b3adc8806a3bf7a1b48bf5
Estevanfranco <drago2062@gmail.com>
2025-05-13 00:13:39 -0500

    api laravel

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => 'Usuario registrado exitosamente', 'user' => $user]);
+    }
+}
---
+++ config/cors.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+'paths' => ['api/*'],
+'allowed_origins' => ['http://localhost:4200'],
+'allowed_methods' => ['*'],
+'allowed_headers' => ['*'],
+
+
+];
